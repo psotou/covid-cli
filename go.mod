801f9becc74b28e4c7efc278e7191e7dc5a3cd96
Pascual Soto Uribe <pascualsu@gmail.com>
2022-01-16 21:53:55 -0300

    feat: adds color and reverse printing order for Comunas

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module covid-data
 
 go 1.16
+
+require github.com/fatih/color v1.13.0
--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
+
+	"github.com/fatih/color"
 )
 
 var (
@@ -14,33 +15,41 @@ var (
 	comuna string
 )
 
+var (
+	title  = color.New(color.FgWhite, color.Bold).Add(color.Underline)
+	fields = color.New(color.FgWhite, color.Bold)
+)
+
 func main() {
 	regionCmd := flag.NewFlagSet("region", flag.ExitOnError)
 	comunaCmd := flag.NewFlagSet("comuna", flag.ExitOnError)
 
-	regionCmd.StringVar(&region, "name", "metropolitana", "region")
-	regionCmd.IntVar(&days, "days", 7, "last given days")
-	comunaCmd.StringVar(&comuna, "name", "nunoa", "region")
-	comunaCmd.IntVar(&days, "days", 7, "last given days")
+	regionCmd.StringVar(&region, "name", "metropolitana", "nombre de la región")
+	regionCmd.IntVar(&days, "days", 7, "número de días")
+	comunaCmd.StringVar(&comuna, "name", "nunoa", "nombre de la comuna")
+	comunaCmd.IntVar(&days, "days", 7, "número de días")
 	flag.Parse()
 
 	args := flag.Args()
 	switch args[0] {
 	case "region":
 		regionCmd.Parse(args[1:])
-
 		casos, _ := CovidRegion(&days, &region)
-		fmt.Printf("%10s %10s %6s\n", "Fecha", "Nacional", strings.Title(region)[0:5])
+		title.Printf("%s: %s\n", "Región", region)
+		fields.Printf("%10s %9s %6s %6s\n", "Fecha", "Nacional", "Casos", "%")
 		for i := range casos.Fechas {
 			nacional, _ := strconv.ParseFloat(casos.Nacional[i], 64)
-			fmt.Printf("%10s %10.f %6s\n", casos.Fechas[i], nacional, casos.Region[i])
+			region, _ := strconv.ParseFloat(casos.Region[i], 64)
+			asPer := (region / nacional) * 100.0
+			fmt.Printf("%10s %9.f %6s %6.1f\n", casos.Fechas[i], nacional, casos.Region[i], asPer)
 		}
 	case "comuna":
 		comunaCmd.Parse(args[1:])
-
 		casos, _ := CovidComuna(&days, &comuna)
-		fmt.Printf("%10s %6s\n", "Fecha", strings.Title(comuna)[0:5])
-		for i := range casos.Fechas {
+		title.Printf("%s: %s\n", "Comuna", comuna)
+		fields.Printf("%10s %6s\n", "Fecha", "Casos")
+		// prints in reverse since the Comuna object return the data en in reverse order
+		for i := len(casos.Fechas) - 1; i >= 0; i-- {
 			comunal, _ := strconv.ParseFloat(casos.Comuna[i], 64)
 			fmt.Printf("%10s %6.f\n", casos.Fechas[i], comunal)
 		}
